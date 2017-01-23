2a4ab102b61f4df8c55274789ec5e525c4328c55
cbrenneisen <ca.brenneisen@gmail.com>
2017-01-22 20:44:34 -0500

    fixed things

---
+++ Contents.swift
@@ -0,0 +1,53 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+func countNucleotides(){
+
+    let d = DNA(sequence: "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC")
+
+    let result = BioService.nucleobaseCount(dna: d)
+    let output = result.map{ String($0.1)}.joined(separator: " ")
+    print(output)
+
+}
+
+func transcribeDNA(){
+    
+    let d = DNA(sequence: "GATGGAACTTGACTACGTAAATT")
+    
+    let r = BioService.transcribe(dna: d)
+    print(r.sequence)
+    
+}
+
+func reverseComplement(){
+    
+    let d = DNA(sequence: "AAAACCCGGT")
+    
+    let d2 = BioService.reverseComplement(dna: d)
+    print(d2.sequence)
+    
+}
+
+func fibonacci(){
+    
+    let result = MathService.fibonacci(n: 5, k: 3)
+    print(result)
+    
+}
+
+func gcContent(){
+    
+    let allDNA = FastaService.loadDNA(fastaFile: "fasta1")
+    for dna in allDNA {
+        print(dna.id!)
+        print(dna.sequence)
+    }
+}
+
+countNucleotides()
+transcribeDNA()
+reverseComplement()
+fibonacci()
+gcContent()
---
+++ Sources/BioService.swift
@@ -0,0 +1,47 @@
+//
+//  BioService.swift
+//  BioSwift
+//
+//  Created by Carlos Brenneisen on 01/21/17.
+//  Copyright © 2017 CarlosBrenneisen. All rights reserved.
+//
+
+import Foundation
+
+public class BioService {
+    
+    //given a DNA strand, return a dictionary containing the count of each base
+    public class func nucleobaseCount(dna: DNA) -> [String: Int]{
+        
+        //valid nucleobases
+        var bases = ["A": 0, "C": 0, "G": 0, "T": 0]
+        
+        for n in dna.sequence.characters {
+            
+            guard let b = bases[String(n)] else {
+                //nucleotide could potentially be invalid
+                continue
+            }
+            //increment the count
+            bases[String(n)] = b + 1
+        }
+        return bases
+    }
+    
+    //given a DNA strand, return the transcribed RNA strand
+    public class func transcribe(dna: DNA) -> RNA {
+        
+        let seq = String(dna.sequence.characters.map{ $0 != "T" ? $0 : "U" })
+        return RNA(sequence: seq)
+        
+    }
+    
+    //given a DNA strand, return the reverse complement
+    public class func reverseComplement(dna: DNA) -> DNA {
+        
+        let complements = ["A": "T", "T": "A", "G": "C", "C": "G"]
+        let seq = dna.sequence.characters.reversed().map({ complements[String($0)]! }).joined(separator: "")
+        return DNA(sequence: seq)
+    }
+    
+}
