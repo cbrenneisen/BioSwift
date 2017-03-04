02feba9f0db5690ba8368afbb759ce4efc1acd08
cbrenneisen <ca.brenneisen@gmail.com>
2017-03-04 14:30:20 -0500

    Initial Commit

---
+++ MendelianSwift/MendelianSwift.h
@@ -0,0 +1,19 @@
+//
+//  MendelianSwift.h
+//  MendelianSwift
+//
+//  Created by Carlos Brenneisen on 3/4/17.
+//  Copyright © 2017 carlosbrenneisen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MendelianSwift.
+FOUNDATION_EXPORT double MendelianSwiftVersionNumber;
+
+//! Project version string for MendelianSwift.
+FOUNDATION_EXPORT const unsigned char MendelianSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MendelianSwift/PublicHeader.h>
+
+
