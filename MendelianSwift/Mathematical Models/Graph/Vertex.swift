fc25abb5e026dd6b977c94726b59ec2ac627b104
cbrenneisen <ca.brenneisen@gmail.com>
2017-03-12 21:19:34 -0400

    Added missing files

---
+++ MendelianSwift/Mathematical Models/Graph/Vertex.swift
@@ -0,0 +1,26 @@
+//
+//  Vertex.swift
+//  MendelianSwift
+//
+//  Created by Carlos Brenneisen on 3/12/17.
+//  Copyright © 2017 carlosbrenneisen. All rights reserved.
+//
+
+import Foundation
+
+public class Vertex<T: BioSequence>: Hashable {
+    
+    public let bioSequence: T
+    
+    internal init(bioSequence: T){
+        self.bioSequence = bioSequence
+    }
+    
+    public var hashValue: Int {
+        return self.bioSequence.hashValue
+    }
+    
+    public static func == (lhs: Vertex<T>, rhs: Vertex<T>) -> Bool {
+        return lhs.bioSequence.sequence == rhs.bioSequence.sequence
+    }
+}
